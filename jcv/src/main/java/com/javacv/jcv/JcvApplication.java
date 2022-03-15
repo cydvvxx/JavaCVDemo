44995144064091b04c267e483edf8c4b3a84db62
cyd <cydvvxx@sina.com>
2022-03-15 23:40:13 +0800

    截取视频帧图片

---
+++ jcv/src/main/java/com/javacv/jcv/JcvApplication.java
@@ -0,0 +1,13 @@
+package com.javacv.jcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcvApplication.class, args);
+    }
+
+}
---
+++ jcv/src/test/java/com/javacv/jcv/JcvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacv.jcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
