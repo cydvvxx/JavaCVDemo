a109dfe0e5f097519a74284a906d61673c80a3d1
cydvvxx <cydvvxx@163.com>
2022-03-16 01:07:22 +0800

    截取视频帧图片并进行等比缩放

--- jcv/src/main/java/com/javacv/jcv/video/ReadVideoInfo.java
+++ jcv/src/main/java/com/javacv/jcv/video/ReadVideoInfo.java
@@ -5,9 +5,11 @@ import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.Buffer;
 
 public class ReadVideoInfo {
 
@@ -47,7 +49,46 @@ public class ReadVideoInfo {
         return null;
     }
 
+    public static void getVideoFrame(String sourcePath,String targetFile,int n,int newWidth) throws IOException {
+        File sourceFile = new File(sourcePath);
+        if (sourceFile.exists()){
+            System.out.println("file exists");
+            FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(sourceFile);
+            grabber.start();
+
+            int length = grabber.getLengthInFrames();
+            int flag = 0;
+            Frame frame = null;
+            while (flag <= length){
+                if (flag > n){
+                    break;
+                }
+                frame = grabber.grabImage();
+                if (frame == null){
+                    break;
+                }
+                Java2DFrameConverter java2DFrameConverter = new Java2DFrameConverter();
+                BufferedImage c = java2DFrameConverter.convert(frame);
+                int imageWidth = grabber.getImageWidth();
+                int imageHeight = grabber.getImageHeight();
+                int newHeight = (int)(((double)newWidth/imageWidth)*imageHeight);
+                BufferedImage bufferedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_3BYTE_BGR);
+                bufferedImage.getGraphics().drawImage(c.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH),0,0,null);
+                String TF= targetFile+flag+".jpg";
+                File targetTile = new File(TF);
+                if (!targetTile.getParentFile().exists()){
+                    System.out.println("parent-file="+targetTile.getParentFile());
+                    targetTile.getParentFile().mkdir();
+                }
+                ImageIO.write(bufferedImage,"jpg",targetTile);
+                flag++;
+            }
+        }
+    }
+
+
     public static void main(String[] args) throws IOException {
-        getTargetPath("D:/study/test/sishi","E:/file/sishi.mp4",10);
+       // getTargetPath("D:/study/test/sishi","E:/file/
+        getVideoFrame("E:/迅雷下载/sishi.mp4","D:/study/test/sishi",100,300);
     }
 }
